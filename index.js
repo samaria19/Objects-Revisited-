ab257b23657c38281c05c84602c553e3e9458df4
Jonathan Corley <jcorley@noscihigh.org>
2021-03-25 15:36:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//Create a variable called myObject and assign 
+//an object to it.
+
+
+
+
+//Add two properties of firstName and lastName
+//using dot notation
+
+
+
+
+//Add two properties of hobbies and age 
+//using bracket notation
+//hobbies must be an array of at least 4 strings
+
+
+
+
+//Print the myObject variable
+
+
+
+
+//Use a for in loop to print all of the keys of the object
+
+
+
+
+///Use a for in loop to print all of the values of the object
+
+
+
+
+
+/*
+create an object that is about a specific car
+
+Include: year, make, model, color and an array of features that you want your car to have
+
+*/
