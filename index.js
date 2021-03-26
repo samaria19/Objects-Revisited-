a82f4446aeadc1d0c9ef8cd43cda2a3cc2f90d2d
Samaria Smothers <ssmothers457@noscihigh.org>
2021-03-26 16:31:04 +0000

    //

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Create a variable called myObject and assign 
 //an object to it.
 
-
-
+var myObject = {};
 
 //Add two properties of firstName and lastName
 //using dot notation
+myobject.firstName
+myobject.lastName
 
 
 
@@ -14,22 +15,27 @@
 //using bracket notation
 //hobbies must be an array of at least 4 strings
 
-
+myObject['age']= 16
+myObject['hobbies']=['reading', 'poetry', 'movies','facetime']
 
 
 //Print the myObject variable
-
+console.log(myObject)
 
 
 
 //Use a for in loop to print all of the keys of the object
 
-
+for(var key in myobject){
+  console.log(key)
+}
 
 
 ///Use a for in loop to print all of the values of the object
 
-
+for(var key in myObject){
+  console.log(myObject[key])
+}
 
 
 
@@ -39,3 +45,10 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+var myCar = {
+  year:2009
+  make:'Nissan'
+  model:'Altuma'
+  color:'light blue'
+  features:['gps', 'radio', 'super speed','4 door']
+}
